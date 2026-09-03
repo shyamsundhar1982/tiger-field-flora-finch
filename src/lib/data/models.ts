331292dddf0fb9837b6e3606555ecc77fd999f58
shyamsundhar1982 <shyamsundhar1982@gmail.com>
2026-09-03 09:26:26 +0530

    Create models.ts

---
+++ src/lib/data/models.ts
@@ -0,0 +1,64 @@
+export const MODELS = [
+  {
+    id: "core-tiagra",
+    tier: "core",
+    name: "Core Tiagra",
+    asp: 111900,
+  },
+  {
+    id: "core-105",
+    tier: "core",
+    name: "Core 105",
+    asp: 145300,
+  },
+  {
+    id: "core-105-elite",
+    tier: "core",
+    name: "Core 105 Elite",
+    asp: 158100,
+  },
+
+  {
+    id: "pro-105-di2",
+    tier: "pro",
+    name: "Pro 105 Di2",
+    asp: 215000,
+  },
+  {
+    id: "pro-rival-axs",
+    tier: "pro",
+    name: "Pro Rival AXS",
+    asp: 205000,
+  },
+  {
+    id: "pro-ultegra-di2",
+    tier: "pro",
+    name: "Pro Ultegra Di2",
+    asp: 255000,
+  },
+  {
+    id: "pro-force-axs",
+    tier: "pro",
+    name: "Pro Force AXS",
+    asp: 245000,
+  },
+
+  {
+    id: "apex-ultegra-di2",
+    tier: "apex",
+    name: "Apex Ultegra Di2",
+    asp: 295000,
+  },
+  {
+    id: "apex-duraace-di2",
+    tier: "apex",
+    name: "Apex Dura-Ace Di2",
+    asp: 395000,
+  },
+  {
+    id: "apex-red-axs",
+    tier: "apex",
+    name: "Apex RED AXS",
+    asp: 425000,
+  },
+];
