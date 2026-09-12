d0686f3e254628120429d9aee06efd246e65ef30
shyamsundhar1982 <shyamsundhar1982@gmail.com>
2026-09-12 15:19:52 +0530

    Type exported HiGHS runtime Wasm module

--- src/highs-wasm.d.ts
+++ src/highs-wasm.d.ts
@@ -2,3 +2,8 @@ declare module "*.wasm?module" {
   const wasmModule: WebAssembly.Module;
   export default wasmModule;
 }
+
+declare module "highs/runtime?module" {
+  const wasmModule: WebAssembly.Module;
+  export default wasmModule;
+}
